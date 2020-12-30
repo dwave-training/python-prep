cad12cdc6176434836cf8c1c32a656d54e7a1b5d
Joel M. Gottlieb <jgottlieb@dwavesys.com>
2020-12-30 08:03:45 -0500

    Removing Months, Days

--- pythonprep_assignment.py
+++ pythonprep_assignment.py
@@ -18,13 +18,11 @@ def define_objects():
     # months. Please use three-character month names, like "Jan" for January.
     # Please call it "Months".
 
-    Months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 
     # TODO: Add code here to define a dictionary containing the days of the
     # month for your current year. Please use three-character month names, like
     # "Jan" for "January". Please call it "Days".
 
-    Days = {'Jan': 31, 'Feb': 28, 'Mar': 31, 'Apr': 30, 'May': 31, 'Jun': 30, 'Jul': 31, 'Aug': 31, 'Sep': 30, 'Oct': 31, 'Nov': 30, 'Dec': 31}
 
     return Months, Days
 
